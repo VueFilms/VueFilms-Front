753893b497f398c179a82c050e6026504bab6f3b
yerayjimenezc <yerayjimenezcarmona@gmail.com>
2022-09-21 12:34:13 +0200

    Fix Search alternative

--- pages/search/_value.vue
+++ pages/search/_value.vue
@@ -1,10 +1,10 @@
 <template>
     <v-container fluid>
-        <v-col cols="12" :v-if="movies" class="d-flex flex-wrap justify-center">
+        <v-col cols="12" v-if="movies" class="d-flex flex-wrap justify-center">
             <singleCard v-for="(movie, idx) in movies" :key="idx" :movie="movie"></singleCard>
         </v-col>
-        <v-col :v-if="!movies">
-            <h1>Ups. 😱 El titulo buscado no existe..</h1>
+        <v-col>
+            <h1 v-if="movies.length === 0">Ups. 😱 El titulo buscado no existe..</h1>
             <v-text-field class="mt-5" label="Buscar Pelicula" v-model="value" filled rounded dense
                 append-icon="mdi-movie-search" @keyup.enter.exact="search" @click:append="search">
             </v-text-field>
@@ -26,19 +26,18 @@ export default {
         return {
             movies: false,
             title: null,
+            value: ''
         }
     },
     methods: {
         search() {
             this.$router.push(`/search/${this.value}`)
             this.value = ''
-
         }
     },
     async mounted() {
         this.title = await this.$route.params.value
         this.movies = await API.getMovieSearch(this.title)
-        console.log(this.movies)
     }
 }
 </script>
