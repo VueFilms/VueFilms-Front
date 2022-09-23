fb65ae81e27a951a56224b2a451bb6c424c1e9ce
yerayjimenezc <yerayjimenezcarmona@gmail.com>
2022-09-23 21:35:19 +0200

    Menu design responsive

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+    <v-navigation-drawer v-model="drawer" :clipped="clipped" fixed app>
       <v-list>
         <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-item-action>
@@ -32,8 +32,16 @@
 
 
     <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <img id="logo" @click="$router.push('/home')" src="../assets/logo.svg">
+      <v-app-bar-nav-icon v-if="show" @click.stop="drawer = !drawer" />
+      <v-container class="d-flex align-center">
+        <img id="logo" @click="$router.push('/home')" src="../assets/logo.svg">
+        <v-spacer></v-spacer>
+        <v-col v-if="!show" cols="8" class="d-flex ">
+          <v-btn v-for="(item, i) in items" :key="i" :to="item.to" color="transparent" elevation="0" router exact>
+            {{item.title}}
+          </v-btn>
+        </v-col>
+      </v-container>
     </v-app-bar>
 
     <v-main>
@@ -118,7 +126,15 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js',
+    }
+  },
+  computed: {
+    show() {
+      if (this.$vuetify.breakpoint.name === 'xs') {
+        return true
+      } else {
+        return false
+      }
     }
   },
   methods: {
